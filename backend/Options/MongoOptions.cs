76c1106f34488535b0b2e4e0cf06bef29e8c4e29
Floris Westerman <me@floriswesterman.nl>
2020-09-28 02:30:47 +0200

    Added a database seed and the backend can now connect to the mongodb replicaset

--- backend/Options/MongoOptions.cs
+++ backend/Options/MongoOptions.cs
@@ -7,14 +7,21 @@ namespace CorPool.BackEnd.Options {
         public string Username { get; set; }
         public string Password { private get; set; }
 
+        public string ReplicaSetName { get; set; } = null;
         public string CredentialsDatabaseName { get; set; } = "admin";
         public string DatabaseName { get; set; }
 
         public MongoClientSettings GetSettings() {
-            return new MongoClientSettings {
+            var settings = new MongoClientSettings {
                 Credential = MongoCredential.CreateCredential(CredentialsDatabaseName, Username, Password),
                 Server = new MongoServerAddress(Host, Port)
             };
+
+            if (!string.IsNullOrWhiteSpace(ReplicaSetName)) {
+                settings.ReplicaSetName = ReplicaSetName;
+            }
+
+            return settings;
         }
     }
 }
