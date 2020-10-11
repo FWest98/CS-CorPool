a65f26f9512289a889e14a16149b35341dd34be5
Floris Westerman <me@floriswesterman.nl>
2020-10-12 00:12:10 +0200

    Add Health check to backend

---
+++ backend/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CorPool.BackEnd.Controllers {
+    [ApiController]
+    [Route("/health")]
+    public class HealthController : ControllerBase {
+        public string Get() {
+            return "OK";
+        }
+    }
+}
